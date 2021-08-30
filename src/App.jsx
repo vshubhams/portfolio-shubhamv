e74e367adeecd674349e487e1e97b67a27cd081c
shubham <shubhamv430@gmail.com>
2021-08-30 14:34:37 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <h1>shubham</h1>
+    </div>
+  );
+}
+
+export default App;
