f511954cc63bbbdab36efb32aca5f3f1b6833480
shubham <shubhamv430@gmail.com>
2021-09-04 01:13:32 +0530

    Resume button

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import styles from "./profile.module.css"
+import GetAppIcon from '@material-ui/icons/GetApp';
 export const Profile = () => {
     return (
         <div className={styles.wrapper}>
@@ -10,7 +11,7 @@ export const Profile = () => {
                 <p className={styles.descrip}>
                     An enthusiastic Full-Stack Web Developer with a strong set of technical as well as non-technical skills and a dedication towards creating useful and interactive web application.
                 </p>
-                <button onClick={()=>{window.open("https://drive.google.com/file/d/1fZG3oHKl-w7_pPZhHlJxTu7EocHZixVM/view?usp=sharing")}}>Download Resume</button>
+                <button onClick={()=>{window.open("https://drive.google.com/file/d/1fZG3oHKl-w7_pPZhHlJxTu7EocHZixVM/view?usp=sharing")}}> <GetAppIcon /> RESUME</button>
             </div>
         </div>
     )
